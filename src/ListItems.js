a7167254873e1a587c1ddd221a8e2d28bf9691cd
Tmcreative1 <tima.mamedov1@gmail.com>
2020-08-22 19:49:46 +0300

    init commit

---
+++ src/ListItems.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './ListItems.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import FlipMove from "react-flip-move";
+
+function ListItems(props) {
+    const listItems = props.items.map(item => {
+        return <div className="list" key={item.key}>
+            <p>
+                <input type="text" id={item.key} value={item.text}
+                       onChange={(e) => props.editItem(e.target.value, item.key)}/>
+                <span>
+                    <FontAwesomeIcon className="faicons" icon="trash"
+                                     onClick={() => props.deleteItem(item.key)}/>
+                </span>
+            </p>
+        </div>
+    })
+
+    return (
+        <div>
+            <FlipMove duration={500} easing="ease-in-out">
+                {listItems}
+            </FlipMove>
+        </div>
+    )
+}
+
+export default ListItems;
